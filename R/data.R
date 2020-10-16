581a11720292bfc8d94fcc65945677eaea0df89f
RodolfoPelinson <rodolfopelinson@gmail.com>
2020-10-16 14:25:55 -0300

    Adding Trait Data

--- R/data.R
+++ R/data.R
@@ -35,3 +35,30 @@
 #' @format Factor w/ 12 levels and 44 observations
 #' @source {Experimental Data}
 "All"
+
+
+#' Traits and other relevant information considering the second and third surveys only
+#'
+#' Traits and other relevant information about each taxa sampled
+#'
+#' @format A data frame with 36 rows or 'observations' and 16 variables:
+#' \describe{
+#'   \item{species}{Factor variable: Ids of the sampled taxa}
+#'   \item{trophic}{Factor variable: Trophic level of the sampled taxa}
+#'   \item{microhabitat}{Factor variable: Microhabitat level of the sampled taxa}
+#'   \item{volume}{Numeric variable: Volume of the largest individual of each sampled taxa}
+#'   \item{mass}{Numeric variable: Wet mass of the largest individual of each sampled taxa}
+#'   \item{volume_log}{Numeric variable: Log transformed volume of the largest individual of each sampled taxa}
+#'   \item{family}{Character variable: Family of each sampled taxa}
+#'   \item{order}{Character variable:  Order of each sampled taxa}
+#'   \item{total_ab}{Numeric variable: Total abundance of each sampled taxa considering all surveys}
+#'   \item{total_ab_log}{Numeric variable: Log transformed total abundance of each sampled taxa considering all surveys}
+#'   \item{total_ab_SS1}{Numeric variable: Total abundance of each sampled taxa considering the first survey}
+#'   \item{total_ab_SS2}{Numeric variable: Total abundance of each sampled taxa considering the second survey}
+#'   \item{total_ab_SS3}{Numeric variable: Total abundance of each sampled taxa considering the third survey}
+#'   \item{total_ab_SS1_log}{Numeric variable: Log transformed total abundance of each sampled taxa considering the first survey}
+#'   \item{total_ab_SS2_log}{Numeric variable: Log transformed total abundance of each sampled taxa considering the second survey}
+#'   \item{total_ab_SS3_log}{Numeric variable: Log transformed total abundance of each sampled taxa considering the third survey}
+#' }
+#' @source {Experimental Data}
+"Trait_SS2_SS3"
