8ada520d2f8d50c18a473fba00ddea6931408375
RodolfoPelinson <rodolfopelinson@gmail.com>
2020-10-16 12:31:57 -0300

    Adding data for community size analyses

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Abundances for the second and third surveys
+#'
+#'
+#' @format Numerical vector w/ with 44 observations
+#' @source {Experimental Data}
+"com_SS2_SS3_abundance"
+
+
+#' Pond IDs for the second and third surveys
+#'
+#' @format Factor w/ 24 levels and 44 observations
+#' @source {Experimental Data}
+"ID_SS2_SS3"
+
+#' Sampling Survey identities for the second and third surveys
+#'
+#' @format Factor w/ 2 levels: "1","2", and 44 observations
+#' @source {Experimental Data}
+"SS_SS2_SS3"
+
+#' Isolation identities for the second and third surveys
+#'
+#' @format Factor w/ 3 levels: "30","120", "480" and 44 observations
+#' @source {Experimental Data}
+"isolation_SS2_SS3"
+
+#' Fish treatment identities for the second and third surveys
+#'
+#' @format Factor w/ 2 levels: "absent","present", and 44 observations
+#' @source {Experimental Data}
+"fish_SS2_SS3"
+
+#' Fish, isolation and surveyidentities combined for the second and third surveys
+#'
+#' @format Factor w/ 12 levels and 44 observations
+#' @source {Experimental Data}
+"All"
